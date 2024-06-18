45fc8e088a4996ecbaba26ccd5fe3d4cd15b88fc
Óscar Alemán <oscaraleman.dev@gmail.com>
2024-06-18 03:19:24 -0600

    Adding Authentication with Cognito

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,25 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "superexpress4d10d861": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "superexpress4d10d861CustomMessage": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
   "hosting": {
     "S3AndCloudFront": {
       "CloudFrontDistributionID": "string",
--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.png';
-import './App.css';
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const App = () => {
   return (
-    <div className="App">
-    <p>Contáctanos al WhatsApp: +506 7060-8880</p>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Super Express</h1>
-      </header>
-    </div>
-  );
+    <Authenticator>
+      {({user}) => (
+        <main>
+          <h1>Bienvenido {user.username}</h1>
+        </main>
+      )}
+    </Authenticator>
+  )
 }
 
 export default App;
