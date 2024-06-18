54acc4b219dfe7b74b3d4f083e84d7cd81983e24
Óscar Alemán <oscaraleman.dev@gmail.com>
2024-06-18 03:26:39 -0600

    Return header

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+import logo from './logo.png';
+import './App.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 const App = () => {
   return (
-    <Authenticator>
-      {({user}) => (
-        <main>
-          <h1>Bienvenido {user.username}</h1>
-        </main>
-      )}
-    </Authenticator>
+    <div>
+      <p>Contáctanos al WhatsApp: +506 7060-8880</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Super Express</h1>
+      </header>
+      <Authenticator>
+        {({user}) => (
+          <main>
+            <h1>Bienvenido {user.username}</h1>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   )
 }
 
